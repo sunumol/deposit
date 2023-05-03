27f70fb3c78caa199e28d3618ca4336236158533
@reshkr <reshma.kr@iroidtechnologies.com>
2023-05-03 18:32:45 +0530

    Firebase index

--- index.js
+++ index.js
@@ -6,10 +6,28 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import messaging from '@react-native-firebase/messaging';
+import * as RootNavigation from './Router/RootNavigation';
 
 // Register background handler
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Message handled in the background!', remoteMessage);
+  messaging()
+    .getInitialNotification()
+    .then(remoteMessage => {
+      if (remoteMessage) {
+        console.log(
+          'Notification caused app to open from quit state:',
+          remoteMessage.notification,
+        );
+        if (remoteMessage.notification?.title === 'DATA_CONFIRMATION') {
+   
+          RootNavigation.navigate('Proceed',{status:true,AcyivityId: remoteMessage.notification?.data?.activityId});
+        }
+        if (remoteMessage.notification?.title === 'DATA_CORRECTION_REQUEST') {
+          RootNavigation.navigate('CorrectionScreen', { AcyivityId: remoteMessage.notification?.data?.activityId });
+        }
+      }
+    });
 });
 
 AppRegistry.registerComponent(appName, () => App);
